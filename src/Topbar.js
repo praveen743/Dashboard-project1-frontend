5d682fcbff169e04105316d73870d899e69ea9c8
PRAVEENRAJ <92292612+praveen743@users.noreply.github.com>
2022-04-04 16:41:58 +0530

    Update Topbar.js

--- src/Topbar.js
+++ src/Topbar.js
@@ -34,7 +34,7 @@ var navigate = useNavigate();
              
              <button className="btn" id='profile'  onClick={() => handleLogout()}>
                         Logout</button>
-                {user!==null?<button className="btn" id='profile'>{user}</button>
+                {user != null?<button className="btn" id='profile'>{user}</button>
                 : <Link to='/login'><button className="btn" id='profile'>Login
                 </button></Link> }
                    
